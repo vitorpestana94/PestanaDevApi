d5dd0a271eec10fd639350c17faba707ef4aba1b
Vitor Pestana <vitor.pestana@prion.com.br>
2026-07-21 10:53:58 -0300

    agora, caso o email já tenha sido confirmado será retornado um erro explícito

--- PestanaDevApi/Constants/Messages/ErrorMessages.ConfirmationCode.cs
+++ PestanaDevApi/Constants/Messages/ErrorMessages.ConfirmationCode.cs
@@ -4,5 +4,6 @@
     {
         public const string InvalidCode = "3F8D : Invalid Code - the provided code doest not match.";
         public const string UnfreshCode = "B71A : Unfresh Code - the provided code is not fresh.";
+        public const string EmailAlreadyConfirmed = "4E28 : Email Already Confirmed - the provided email was already confirmed!";
     }
 }
